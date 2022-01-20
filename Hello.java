a8e9655d4d9e01eb0a08b6353770312123e30e0a
Joseph Maradiaga <jmaradiaga901@gmail.com>
2022-01-20 12:26:23 -0500

    The New chnages

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 
 public class Hello
 {
     public static void main(String[] args)
     {
 	System.out.println("Hello Mx.Willow");
-	java.util.Scanner keyboard = new java.util.Scanner(System.in);
+	Scanner keyboard = new java.util.Scanner(System.in);
 	System.out.println("How are you?");
 	String response = keyboard.nextLine();
         System.out.println("How is the weather outside");
