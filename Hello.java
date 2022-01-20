1bce8ec06820c65553854a0d94f9370100ae0dde
Joseph Maradiaga <jmaradiaga901@gmail.com>
2022-01-20 12:18:10 -0500

    we are still working

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello
     public static void main(String[] args)
     {
 	System.out.println("Hello Mx.Willow");
+	java.util.Scanner keyboard = new java.util.Scanner(System.in);
+	System.out.println("How are you?");
+	String response = keyboard.nextLine();
     }
 }
 
